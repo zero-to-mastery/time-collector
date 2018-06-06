e2a6607bd723aa1ab02b375418d655a7675c7b58
w3bh4ck <transamadi.lucky@gmail.com>
2018-06-06 12:26:33 +0100

    To be added as contributor

--- bootstrap/js/bootstrap.js
+++ bootstrap/js/bootstrap.js
@@ -3,6 +3,7 @@
   * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
 	typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
