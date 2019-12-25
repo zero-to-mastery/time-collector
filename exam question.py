b37f1eb00acbee63ee6d98a2f15727bfcea9af42
Bhorlarh <48389765+Bhorlarh@users.noreply.github.com>
2019-12-25 23:20:22 +0100

    styling save-to-project popup

---
+++ exam question.py
@@ -0,0 +1,14 @@
+import math
+
+sum = 0;
+inputData = (str(open('data.txt', 'r').read())).split("\n")
+
+values = (math.floor(n/3) - 2)
+while values
+
+for n in inputData[0:100]:
+    n = int(n)
+    values = (math.floor(n/3) - 2)
+    sum += values
+
+print(sum)
